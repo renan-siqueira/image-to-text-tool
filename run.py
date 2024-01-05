d19d2a7e17e68093c66b51a44bd95fcb38de6954
Renan Siqueira Antonio <renan.siqu@gmail.com>
2024-01-05 20:25:59 -0300

    feat: refactoring the main file by moving aux functions to utils.py

--- run.py
+++ run.py
@@ -2,11 +2,9 @@
     Execution project file
 '''
 import os
-import json
-import datetime
-
-from src.modules import blip_model
 from src.settings import config
+from src.modules import blip_model
+from src.utils import utils
 
 
 def process_images(model, processor, device, input_folder):
@@ -19,30 +17,10 @@ def process_images(model, processor, device, input_folder):
     return descriptions
 
 
-def read_json_file(filepath):
-    if os.path.exists(filepath):
-        with open(filepath, "r", encoding='utf-8') as file:
-            return json.load(file)
-    return []
-
-
-def write_json_file(filepath, new_data):
-    data = read_json_file(filepath)
-    data.append(new_data)
-    with open(filepath, "w", encoding='utf-8') as file:
-        json.dump(data, file, indent=4)
-
-
 def main():
     model, processor, device = blip_model.load_model()
     image_descriptions = process_images(model, processor, device, config.APP_PATH_INPUT_FOLDER)
-    
-    new_execution = {
-        "date": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-        "descriptions": image_descriptions
-    }
-
-    write_json_file(config.APP_PATH_OUTPUT_JSON_FILE, new_execution)
+    utils.write_json_file(config.APP_PATH_OUTPUT_JSON_FILE, image_descriptions)
 
 
 if __name__ == "__main__":
