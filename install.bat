43f06eb822340bb545d7668759d3d38776f24776
Renan Siqueira Antonio <renan.siqu@gmail.com>
2024-01-05 22:25:49 -0300

    feat: create install file to windows system

---
+++ install.bat
@@ -0,0 +1,19 @@
+@echo off
+
+echo Creating virtual environment...
+python -m venv venv
+
+echo Activating virtual environment...
+call venv\Scripts\activate.bat
+
+echo Installing dependencies from requirements.txt...
+pip install -r requirements.txt
+
+echo Installing PyTorch with GPU support...
+pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu121
+
+echo Creating 'input' folder...
+if not exist "input" mkdir input
+
+echo Installation completed.
+pause
