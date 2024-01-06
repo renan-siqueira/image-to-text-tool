696a6c9071a5abdaa81fbf2d99cce82e005ac5b4
Renan Siqueira Antonio <renan.siqu@gmail.com>
2024-01-05 22:25:35 -0300

    feat: create install file to linux system

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Creating virtual environment
+echo "Creating virtual environment..."
+python3 -m venv venv
+
+# Activating virtual environment
+echo "Activating virtual environment..."
+source venv/bin/activate
+
+# Installing dependencies
+echo "Installing dependencies from requirements.txt..."
+pip install -r requirements.txt
+
+# Installing custom version of PyTorch
+echo "Installing PyTorch with GPU support..."
+pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu121
+
+# Creating 'input' folder
+echo "Creating 'input' folder..."
+mkdir -p input
+
+echo "Installation completed."
